065f8aadf8675909ff6e87709002977a9aa60f06
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-03-13 09:08:09 -0500

    update types

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -1,6 +1,40 @@
 import { Type, validator, validators } from '@openmrs/esm-framework';
 import _default from 'yup/lib/locale';
 
+//actualizar
+export interface LocationResponse {
+  type: string;
+  total: number;
+  resourceType: string;
+  meta: {
+    lastUpdated: string;
+  };
+  link: Array<{
+    relation: string;
+    url: string;
+  }>;
+  id: string;
+  entry: Array<LocationEntry>;
+}
+
+export interface LocationEntry {
+  resource: Resource;
+}
+
+export interface Resource {
+  id: string;
+  name: string;
+  resourceType: string;
+  status: 'active' | 'inactive';
+  meta?: {
+    tag?: Array<{
+      code: string;
+      display: string;
+      system: string;
+    }>;
+  };
+}
+
 export interface SectionDefinition {
   id: string;
   name?: string;
