5a0daa5d8f07671b7396f9eaf83e374b383546fe
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-03-13 09:09:07 -0500

    update types

--- src/patient-registration/field/person-attributes/location-person-attribute-field.resource.tsx
+++ src/patient-registration/field/person-attributes/location-person-attribute-field.resource.tsx
@@ -1,7 +1,38 @@
 import { useMemo } from 'react';
 import { type FetchResponse, fhirBaseUrl, openmrsFetch, useDebounce } from '@openmrs/esm-framework';
-import { type LocationEntry, type LocationResponse } from '../../../config-schema';
 import useSWR from 'swr';
+
+export interface LocationResponse {
+  type: string;
+  total: number;
+  resourceType: string;
+  meta: {
+    lastUpdated: string;
+  };
+  link: Array<{
+    relation: string;
+    url: string;
+  }>;
+  id: string;
+  entry: Array<LocationEntry>;
+}
+
+export interface LocationEntry {
+  resource: Resource;
+}
+export interface Resource {
+  id: string;
+  name: string;
+  resourceType: string;
+  status: 'active' | 'inactive';
+  meta?: {
+    tag?: Array<{
+      code: string;
+      display: string;
+      system: string;
+    }>;
+  };
+}
 
 interface UseLocationsResult {
   locations: Array<LocationEntry>;
